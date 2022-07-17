d487c8408eb63812cefb0299ade8ed0791350f65
RaeCoder <raecoder@gmail.com>
2022-07-17 15:04:17 +1000

    Added correct verbose to $roles Fixed permissions errors Bot pings on $exit Bot pings on $makerole Fixed $giverole and added error handling

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import asyncio
 
 #https://discordpy.readthedocs.io/en/latest/index.html
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print("We have logged in as {}".format(client.user))
+    print("We have logged in as {}".format(client.user.id))
 
 @client.event
 async def on_message(msg):
@@ -29,6 +31,7 @@ async def on_message(msg):
         await msg.channel.send(msg.content)
 
     if msg.content.startswith("$exit"):
+        await msg.channel.send("Exiting")
         await client.close()
 
     if msg.content.startswith("$delete-some"):
@@ -46,21 +49,35 @@ async def on_message(msg):
     if msg.content.startswith("$roles"):
         await msg.channel.send("All roles are: {}".format(msg.author.roles))
         await asyncio.sleep(0.5)
-        await msg.channel.send("The name of the top role is: {}".format(msg.author.top_role.name))
+        await msg.channel.send("The name of the user's top role is: {}".format(msg.author.top_role.name))
         await asyncio.sleep(0.5)
         await msg.channel.send("User is administrator: {}".format(msg.author.top_role.permissions.administrator))
 
     if msg.content.startswith("$makerole"):
         await msg.guild.create_role(name="yo", permissions=msg.author.top_role.permissions.all())
+        await msg.channel.send("Successfully made role \"yo\"")
 
     if msg.content.startswith("$giverole"):
-    
-        targetID = msg.raw_mentions
-        targetMember = msg.guild.get_member(targetID[0])
+        rawMessageRoleData = msg.raw_role_mentions
+        rawMessageMemberData = msg.raw_mentions
+        try:
+            lolkappa = rawMessageRoleData[0]
+            targetRole = msg.guild.get_role(rawMessageRoleData[0])
+        except:
+            targetRole = None
+        try:
+            lolkappa = rawMessageMemberData[0]
+            targetMember = msg.guild.get_member(rawMessageMemberData[0])
+        except:
+            targetMember = None
+        if targetRole == None:
+            await msg.channel.send("Failed to find Role")
         if targetMember == None:
-            await msg.channel.send("Failed to find user")
-        else:
-            await msg.channel.send(targetMember.name)
+            await msg.channel.send("Failed to find User")
+        elif targetRole != None and targetMember != None:
+            await msg.channel.send("Name: {} || Role: {}".format(targetMember.name, targetRole.name))
+            await targetMember.add_roles(targetRole, atomic=True)
+        
 
 
         
